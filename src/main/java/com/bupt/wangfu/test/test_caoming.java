682806f0107ae2ab2769dd4a471bfd51c1cd147f
maomaoxiaozhang <598360805@qq.com>
2017-03-17 20:02:21 +0800

    caoming

---
+++ src/main/java/com/bupt/wangfu/test/test_caoming.java
@@ -0,0 +1,28 @@
+package com.bupt.wangfu.test;
+
+import java.sql.SQLException;
+
+/**
+ * Created by lenovo on 2017/3/17.
+ * 测试数据库内容
+ */
+public class test_caoming {
+	public static void main(String[] args) throws SQLException {
+//		createAvgSpdByPort();
+//		createSumByPort();
+//		insertAvgSpdByPort(10, "controlid", "switchid", "portid", Long.valueOf(123456789));
+//		insertAvgSpdByPort(10, "controllerid", "switchid", "portid", Long.valueOf(66666));
+//		ResultSet rs = selectAvgSpdByPort(10, "controllerid", "switchid", "portid");
+//		while(rs.next()){
+//			System.out.println(rs.getLong("speed"));
+//		}
+//		insertSumByPort("cid", "switchid", "portid", Long.valueOf(123456));
+//		insertSumByPort("controllerid", "switchid", "portid", Long.valueOf(9999999));
+//		ResultSet rs = selectSumByPort("cid", "switchid", "portid");
+//		while(rs.next()){
+//			System.out.println(rs.getLong("sum"));
+//		}
+//		truncateTable("avgSpdByPort");
+//		truncateTable("sumbyport");
+	}
+}
